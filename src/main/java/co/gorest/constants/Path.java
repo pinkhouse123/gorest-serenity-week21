c381d2b2adc5a45d5418c1f5038e2b42ed4035ea
pinkhouse123 <wembley1400@gmail.com>
2022-06-21 15:19:48 +0100

    Week21-gorest-serenity

---
+++ src/main/java/co/gorest/constants/Path.java
@@ -0,0 +1,5 @@
+package co.gorest.constants;
+
+public class Path {
+    public static final String  USER="/public/v2";
+}
---
+++ src/test/java/co/gorest/testbase/TestBase.java
@@ -0,0 +1,16 @@
+package co.gorest.testbase;
+
+import co.gorest.utils.PropertyReader;
+import co.gorest.constants.Path;
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+
+public class TestBase {
+    public static PropertyReader propertyReader;
+
+    @BeforeClass
+    public static void init() {
+        propertyReader = PropertyReader.getInstance();
+        RestAssured.baseURI = propertyReader.getProperty("baseUrl");
+        RestAssured.basePath = Path.USER;}
+}
